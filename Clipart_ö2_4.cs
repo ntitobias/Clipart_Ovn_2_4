45c4f8c407aa8e07eac37c70452076522047d19f
Tobias Lindberg <tobias.lindberg@ga.ntig.se>
2021-11-11 11:49:07 +0100

    Add project files.

---
+++ Clipart_ö2_4.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clipart_Ovn_2_4
+{
+    class Figur
+    {
+        public float bredd;
+        public float höjd;
+
+        public Figur(float bredd, float höjd)
+        {
+            this.bredd = bredd;
+            this.höjd = höjd;
+        }
+    }
+
+    class Triangel : Figur
+    {
+        public Triangel(float bredd, float höjd) : base(bredd, höjd)
+        {
+        }
+
+        public float BeräknaArea()
+        {
+            return bredd * höjd / 2;
+        }
+    }
+
+    class Cirkel : Figur
+    {
+        public Cirkel(float bredd, float höjd) : base(bredd, höjd)
+        {
+        }
+
+        public float BeräknaArea()
+        {
+            return bredd * (float)Math.PI;
+        }
+    }
+
+    class Linje : Figur
+    {
+        public Linje(float bredd, float höjd) : base(bredd, höjd)
+        {
+        }
+    }
+}
---
+++ Form1.Designer.cs
@@ -0,0 +1,202 @@
+﻿namespace Clipart_Ovn_2_4
+{
+    partial class frmClipart
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gbxSkapa = new System.Windows.Forms.GroupBox();
+            this.lblBredd = new System.Windows.Forms.Label();
+            this.tbxBredd = new System.Windows.Forms.TextBox();
+            this.btnSummeraArea = new System.Windows.Forms.Button();
+            this.btnTriangel = new System.Windows.Forms.Button();
+            this.tbxHöjd = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnCirkel = new System.Windows.Forms.Button();
+            this.btnLinje = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbxFigurer = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tbxArea = new System.Windows.Forms.TextBox();
+            this.gbxSkapa.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gbxSkapa
+            // 
+            this.gbxSkapa.Controls.Add(this.btnLinje);
+            this.gbxSkapa.Controls.Add(this.btnCirkel);
+            this.gbxSkapa.Controls.Add(this.tbxHöjd);
+            this.gbxSkapa.Controls.Add(this.label1);
+            this.gbxSkapa.Controls.Add(this.btnTriangel);
+            this.gbxSkapa.Controls.Add(this.tbxBredd);
+            this.gbxSkapa.Controls.Add(this.lblBredd);
+            this.gbxSkapa.Location = new System.Drawing.Point(13, 13);
+            this.gbxSkapa.Name = "gbxSkapa";
+            this.gbxSkapa.Size = new System.Drawing.Size(225, 251);
+            this.gbxSkapa.TabIndex = 0;
+            this.gbxSkapa.TabStop = false;
+            this.gbxSkapa.Text = "Skapa";
+            // 
+            // lblBredd
+            // 
+            this.lblBredd.AutoSize = true;
+            this.lblBredd.Location = new System.Drawing.Point(6, 38);
+            this.lblBredd.Name = "lblBredd";
+            this.lblBredd.Size = new System.Drawing.Size(46, 17);
+            this.lblBredd.TabIndex = 0;
+            this.lblBredd.Text = "Bredd";
+            // 
+            // tbxBredd
+            // 
+            this.tbxBredd.Location = new System.Drawing.Point(68, 35);
+            this.tbxBredd.Name = "tbxBredd";
+            this.tbxBredd.Size = new System.Drawing.Size(144, 22);
+            this.tbxBredd.TabIndex = 1;
+            // 
+            // btnSummeraArea
+            // 
+            this.btnSummeraArea.Location = new System.Drawing.Point(81, 277);
+            this.btnSummeraArea.Name = "btnSummeraArea";
+            this.btnSummeraArea.Size = new System.Drawing.Size(144, 31);
+            this.btnSummeraArea.TabIndex = 1;
+            this.btnSummeraArea.Text = "Sammanlagd area";
+            this.btnSummeraArea.UseVisualStyleBackColor = true;
+            // 
+            // btnTriangel
+            // 
+            this.btnTriangel.Location = new System.Drawing.Point(68, 113);
+            this.btnTriangel.Name = "btnTriangel";
+            this.btnTriangel.Size = new System.Drawing.Size(144, 31);
+            this.btnTriangel.TabIndex = 2;
+            this.btnTriangel.Text = "Skapa triangel";
+            this.btnTriangel.UseVisualStyleBackColor = true;
+            // 
+            // tbxHöjd
+            // 
+            this.tbxHöjd.Location = new System.Drawing.Point(68, 75);
+            this.tbxHöjd.Name = "tbxHöjd";
+            this.tbxHöjd.Size = new System.Drawing.Size(144, 22);
+            this.tbxHöjd.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Höjd";
+            // 
+            // btnCirkel
+            // 
+            this.btnCirkel.Location = new System.Drawing.Point(68, 161);
+            this.btnCirkel.Name = "btnCirkel";
+            this.btnCirkel.Size = new System.Drawing.Size(144, 31);
+            this.btnCirkel.TabIndex = 4;
+            this.btnCirkel.Text = "Skapa Cirkel";
+            this.btnCirkel.UseVisualStyleBackColor = true;
+            // 
+            // btnLinje
+            // 
+            this.btnLinje.Location = new System.Drawing.Point(68, 207);
+            this.btnLinje.Name = "btnLinje";
+            this.btnLinje.Size = new System.Drawing.Size(144, 31);
+            this.btnLinje.TabIndex = 5;
+            this.btnLinje.Text = "Skapa Linje";
+            this.btnLinje.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(245, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Figurlista";
+            // 
+            // lbxFigurer
+            // 
+            this.lbxFigurer.FormattingEnabled = true;
+            this.lbxFigurer.ItemHeight = 16;
+            this.lbxFigurer.Location = new System.Drawing.Point(245, 51);
+            this.lbxFigurer.Name = "lbxFigurer";
+            this.lbxFigurer.Size = new System.Drawing.Size(195, 212);
+            this.lbxFigurer.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(245, 266);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(123, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Sammanlagd area";
+            // 
+            // tbxArea
+            // 
+            this.tbxArea.Location = new System.Drawing.Point(248, 286);
+            this.tbxArea.Name = "tbxArea";
+            this.tbxArea.Size = new System.Drawing.Size(192, 22);
+            this.tbxArea.TabIndex = 5;
+            // 
+            // frmClipart
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(453, 325);
+            this.Controls.Add(this.tbxArea);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lbxFigurer);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnSummeraArea);
+            this.Controls.Add(this.gbxSkapa);
+            this.Name = "frmClipart";
+            this.Text = "Clipart";
+            this.gbxSkapa.ResumeLayout(false);
+            this.gbxSkapa.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox gbxSkapa;
+        private System.Windows.Forms.Button btnLinje;
+        private System.Windows.Forms.Button btnCirkel;
+        private System.Windows.Forms.TextBox tbxHöjd;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnTriangel;
+        private System.Windows.Forms.TextBox tbxBredd;
+        private System.Windows.Forms.Label lblBredd;
+        private System.Windows.Forms.Button btnSummeraArea;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lbxFigurer;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox tbxArea;
+    }
+}
+
